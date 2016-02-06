83eebdda63fac9a983625656384d6caa8b787814
estroid <estroid@gmail.com>
2016-02-05 23:19:43 -0500

    add jquery

--- js/app.js
+++ js/app.js
@@ -1,61 +1,72 @@
-
-// Declare variable of user's guessed number and secret number; place them at the top of the scope and before other functions
-var guessedNum = x > 0 && x < 100; // Variable is integer greater than 1 and less than 1000
-var secretNum = x > 0 && x < 100; // Variable is integer greater than 1 and less than 1000
-
-// Generates secret number between 1 and 999 that user will have to guess. You should try to start a new game without refreshing or reloading the page.
-function getSecretNumber(min, max) {
-    return Math.random() * (100 - 1) + 1;
-    console.log("picked secret number");
-}
-
 $(document).ready(function(){ // HTML code is completely loaded before running the code here
 
-// --- Display information modal box ---
-    $(".what").click(function(){
-        $(".overlay").fadeIn(1000);
+// Declare variable of user's guessed number and secret number; place them at the top of the scope and before other functions
 
+    var secretNum; // variable with global scope
+
+// Generates secret number between 1 and 999 that user will have to guess. You should try to start a new game without refreshing or reloading the page.
+    function getSecretNumber(min, max) {
+        secretNum = Math.random() * (100 - 1) + 1;
+        return secretNum; // This returns value of secretNum to globally scoped variable
+    }
+
+    getSecretNumber(1, 100); // This is calling/invoking the function getSecretNumber to be used
+
+    var num = Math.round(secretNum); // Declare variable num of secretNum rounded off to nearest integer
+    console.log(num);
+
+// --- Display information modal box ---
+    $('.what').click(function(){
+        $(".overlay").fadeIn(1000);
     });
 
-
 // --- Hide information modal box ---
-    $("a.close").click(function() {
+    $('a.close').click(function() {
         $(".overlay").fadeOut(1000);
     });
 
     $('.guess-form').submit(function(e){
         e.preventDefault(); // Prevents default behavior in html of refreshing page load
-        var guessedNum = $("#userGuess").val(); // Creates a variable called newItem from the value in the input with ID item
+        var guessedNum = $('#userGuess').val(); // Setting a variable called guessedNum which is the value of #userGuess
+        $('#guessList').append('<li>'+ guessedNum +'</li>'); //guessedNum needs to be concatenated because we want the variable of guessedNum and not the string; also shows guessed numbers from #guessList
 
+// HOW TO GET FORM TO CLEAR/RESET EACH TIME?????
+        // $('#userGuess').trigger('reset');
+        console.log(guessedNum);
 
-// --- If-else statement to figure out proximity between secretNum and guessedNum ---
+// --- If-else statement to figure out proximity between rounded off num and guessedNum ---
 
-// Add math.abs
+        var difference = Math.abs(num - guessedNum) // Returns the absolute value of a number, so Math.abs('-1') would return 1
 
         // If secretNum is greater than guessedNum
-        if (secretNum - guessedNum >= 50) { // If the difference is more than 50
-
-            $('.guessList').append('<li>Ice cold!</li>');
+        if (difference >= 50) { // If the difference is more than 50
+            $('#feedback').html('Ice cold!');
             console.log("ice cold");
         }
-        else if (secretNum - guessedNum >= 30) { // If the difference is more than 30
-            $('.guessList').append('<li>Cold!</li>');
+        else if (difference >= 30) { // If the difference is more than 30
+            $('#feedback').html('Cold!');
             console.log("cold");
         }
-        else if (secretNum - guessedNum >= 20) { // If the difference is more than 20
-            $('.guessList').append('<li>Warm!</li>');
+        else if (difference >= 20) { // If the difference is more than 20
+            $('#feedback').html('Warm!');
             console.log("warm");
         }
-        else if (secretNum - guessedNum >= 10) { // If the difference is more than 10
-            $('.guessList').append('<li>Hot!</li>');
+        else if (difference >= 10) { // If the difference is more than 10
+            $('#feedback').html('Hot!');
             console.log("hot");
         }
-        else if (secretNum - guessedNum >= 1) { // If the difference is more than 1
-            $('.guessList').append('<li>Very hot!</li>');
+        else if (difference >= 1) { // If the difference is more than 1
+            $('#feedback').html('Very hot!');
             console.log("very hot");
         }
-    });
+        else { // When user guesses correctly
+            $('#feedback').html('You won!');
+            console.log("You won!");
+        }
+    })
+});
 
+// Add a guess iterator to show user the number of guesses they've made so far!!!!!!
 
 
 
